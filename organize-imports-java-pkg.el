6ee5006e487abc76828113c0c45876b154c53e78
jenchieh <jcs090218@gmail.com>
2018-04-22 11:17:33 +0800

    remove illegal multiple description issue.

--- organize-imports-java-pkg.el
+++ organize-imports-java-pkg.el
@@ -1,8 +1,4 @@
 (define-package
   "organize-imports-java"
   "0.0.1"
-  "Organize Imports Java is an organize imports functionalities
-plugins for editing Java code. The only purpose of this project
-is to implement the functionalities of how eclipse treated as
-C-S-o key. This plugin only uses elisp without using any other
-plugin, so it make this plugin more portable and light weight.")
+  "Organize Imports Java is an organize imports functionalities plugins for editing Java code. The only purpose of this project is to implement the functionalities of how eclipse treated as C-S-o key. This plugin only uses elisp without using any other plugin, so it make this plugin more portable and light weight.")
