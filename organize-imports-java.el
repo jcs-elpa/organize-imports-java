6221c85b37d7baee27288848fa38d5519cdeafd5
JenChieh <jcs090218@gmail.com>
2025-01-01 02:12:22 -0800

    Update copyright year

--- organize-imports-java.el
+++ organize-imports-java.el
@@ -1,6 +1,6 @@
 ;;; organize-imports-java.el --- Automatically organize imports in Java code  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2018-2024  Shen, Jen-Chieh
+;; Copyright (C) 2018-2025  Shen, Jen-Chieh
 ;; Created date 2018-04-16 13:12:01
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
